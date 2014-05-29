0905b17da50d6635ae2b979899d48c50bc07386c
jalvarez <juan.alvarezz@gmail.com>
2014-05-28 23:35:01 -0400

    Module 5: Part 1

---
+++ FailTracker/ActionResults/StandardJsonResult.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FailTracker.ActionResults
+{
+    public class StandardJsonResult : JsonResult
+    {
+        public IList<string> ErrorMessages { get; private set; }
+
+        public StandardJsonResult()
+        {
+            ErrorMessages = new List<string>();
+        }
+
+        public void AddError(string errorMessage)
+        {
+            ErrorMessages.Add(errorMessage);
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context != null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            if(this.JsonRequestBehavior == System.Web.Mvc.JsonRequestBehavior.DenyGet &&
+               string.Equals(context.HttpContext.Request.HttpMethod, "GET", StringComparison.InvariantCulture)){
+                throw new InvalidOperationException("GET access is not allow.");
+            }
+
+            var response = context.HttpContext.Response;
+            response.ContentType = string.IsNullOrEmpty(ContentType) ? "application/json" : "";
+
+            if(ContentEncoding != null){
+                response.ContentEncoding = ContentEncoding;
+            }
+
+            SerializeData(response);
+        }
+
+        protected virtual void SerializeData(HttpResponseBase response)
+        {
+            if (ErrorMessages.Any())
+            {
+                var originalData = Data;
+
+                Data = new
+                {
+                    Success = false,
+                    OriginalData = originalData,
+                    ErrorMessage = string.Join("\n", ErrorMessages),
+                    ErrorMessages = ErrorMessages.ToArray()
+                };
+
+                response.StatusCode = 400;
+            }
+
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new JsonConverter[]{
+                    new StringEnumConverter()
+                }
+            };
+
+            response.Write(JsonConverter.SerializeObject(Data, settings));
+        }
+    }
+}
---
+++ FailTracker/Scripts/app/controllers/editIssueController.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strinct';
+
+    var controllerId = 'editIssueController';
+
+    angular.module('failtrackerApp').controller(controllerId,
+        ['$scope', '$http', editIssueController]);
+
+    function editIssueController($scope, $http) {
+        $scope.editing = false;
+        $scope.init = init;
+        $scope.save = save;
+        $scope.cancel = cancel;
+        $scope.edit = edit;
+
+        $scope.init(issue){
+            $scope.originalIssue = angular.extend({}, issue);
+            $scope.issue = issue;
+        }
+
+        $scope.edit(){
+            $scope.editing = true;
+        }
+
+        $scope.save(){
+            $http.post("/Issue/Edit", $scope.issue)
+                .success(function(data){ 
+                    if(data.success !== true){
+                        alert("There was a problem saving to the server: " + data.errorMessage);
+                        return;
+                    }
+
+                    $scope.originalIssue = angular.extend({}, $scope.issue);
+
+                    $scope.editing = false;
+                })
+                .error(function(){ 
+                    alert("There was a problem saving the issue. Please try again.");
+                });
+        }
+
+        $scope.cancel() { }
+    }
+});
